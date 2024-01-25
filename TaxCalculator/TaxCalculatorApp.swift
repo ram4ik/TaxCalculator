3321ed200f860247b5672edfd3f3585b8a631581
Ramill Ibragimov <ribragimov@malwarebytes.com>
2024-01-25 20:25:08 +0200

    Initial Commit

---
+++ TaxCalculator/TaxCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaxCalculatorApp.swift
+//  TaxCalculator
+//
+//  Created by test on 25.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct TaxCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
