bd32f13e459e1cf1100ff36e6ff44a74f58d224e
Ramill Ibragimov <ribragimov@malwarebytes.com>
2024-01-25 21:30:34 +0200

    simple tax

--- TaxCalculator/CalculatorView.swift
+++ TaxCalculator/CalculatorView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct CalculatorView: View {
     @State private var salary: String = ""
+    @State private var tax = 0.0
     
     var body: some View {
         VStack {
@@ -22,14 +23,25 @@ struct CalculatorView: View {
                 .padding(.bottom, 75)
                 .keyboardType(.decimalPad)
             
+            Text("Tax: \(String(format: "%.2f", tax))")
+            
             Button{
-                
+                if let income = Double(salary) {
+                    tax = calculateTax(for: income)
+                    print(tax)
+                }
             } label: {
                 Text("Calculate Tax")
             }
             .buttonStyle(.borderedProminent)
         }
         .padding()
+    }
+    
+    func calculateTax(for income: Double) -> Double {
+        let taxRate = 0.20
+        let taxAmount = income * taxRate
+        return taxAmount
     }
 }
 
