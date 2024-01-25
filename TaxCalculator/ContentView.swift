973441052cd97a930bbc7883ba86461542be15fa
Ramill Ibragimov <ribragimov@malwarebytes.com>
2024-01-25 20:35:37 +0200

    init

--- TaxCalculator/ContentView.swift
+++ TaxCalculator/ContentView.swift
@@ -8,12 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var salary: String = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Annual Salary")
+            
+            TextField("Salary", text: $salary)
+            
+            Button{
+                
+            } label: {
+                Text("Calculate Tax")
+            }
         }
         .padding()
     }
